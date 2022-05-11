f157a77839b0c9367368fdc609fadc6b0d58318d
BECASS ☠️ <83149214+matheusbecaribr@users.noreply.github.com>
2022-05-11 10:31:04 -0300

    atualization

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 function calcularmedia() {
+    var media = window.document.getElementById('media')
     var m1 = window.document.getElementById('m1')
     var m2 = window.document.getElementById('m2')
     var m3 = window.document.getElementById('m3')
 
+    var m = Number(media.value)
     var n1 = Number(m1.value)
     var n2 = Number(m2.value)
     var n3 = Number(m3.value)
@@ -16,9 +18,10 @@ function calcularmedia() {
 
     res1.innerHTML = 'A média do aluno é ' + r
 
-    if (s >= 7) {
+    if (s >= m) {
         navprincipal.style.display = 'none'
         res.style.display = 'block'
+        res.style.color = 'green'
         res.innerHTML = 'O aluno foi <strong>APROVADO</strong>!'
     }
     else {
